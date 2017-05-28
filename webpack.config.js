0ffcbf4b4a5ed0f008dcb6f6da7e9436067e8b74
Ayan Dalal <adalal4@sapient.com>
2017-05-29 01:05:29 +0530

    removed presets from webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -15,10 +15,7 @@ module.exports = {
         loaders: [{
                 test:/.jsx?$/,
                 exclude:/node_modules/,
-                loader:'babel-loader',
-                // query:{
-                //     presets: ["es2015", "react", "stage-0"]
-                // }
+                loader:'babel-loader'
             }]
     }
 }
